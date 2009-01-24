79f5d40aa78bb71ee6c0766759aa37899393df1e
Andreas Rottmann <rotty@debian.org>
2009-01-24 14:37:58 +0100

    Switch testsuite to (spells testing)
    darcs-hash:20090124133758-b4420-a69fa24bdc149a324969bc18521356cbcbc0a0a2.gz

--- tests/stexi.scm
+++ tests/stexi.scm
@@ -29,14 +29,16 @@
 
 (import (rnrs)
         (spells condition)
-        (only (spells strings) string-null?)
+        (only (srfi :13 strings) string-null?)
         (texinfo private)
-        (test-manager))
+        (spells testing))
 
 (define nl (string #\newline))
 
-(in-test-group read-verbatim-body
-  (define (read-verbatim-body-from-string str)
+(define-test-suite stexi-tests
+  "Texinfo processing")
+
+(define (read-verbatim-body-from-string str)
     (define (consumer fragment foll-fragment seed)
       (cons* (if (equal? foll-fragment (string #\newline))
                  (string-append " NL" nl)
@@ -45,74 +47,75 @@
     (reverse
      (read-verbatim-body (open-string-input-port str) consumer '())))
 
-  (define-each-test
-    (assert-equal
-      '()
-      (read-verbatim-body-from-string "@end verbatim\n"))
+(define-test-case stexi-tests read-verbatim-body ()
 
-    ;; after @verbatim, the current position will always directly after
-    ;; the newline.
+  (test-equal
+   '()
+   (read-verbatim-body-from-string "@end verbatim\n"))
 
-    (assert-exception condition?
-      (read-verbatim-body-from-string "@end verbatim"))
+  ;; after @verbatim, the current position will always directly after
+  ;; the newline.
+  (test-exception condition?
+                  (read-verbatim-body-from-string "@end verbatim"))
 
-    (assert-equal
-      '("@@end verbatim" " NL\n")
-      (read-verbatim-body-from-string "@@end verbatim\n@end verbatim\n"))
+  (test-equal
+   '("@@end verbatim" " NL\n")
+   (read-verbatim-body-from-string "@@end verbatim\n@end verbatim\n"))
 
-    (assert-equal
-     '("@@@@faosfasf adsfas " " NL\n" " asf @foo{asdf}" " NL\n")
-     (read-verbatim-body-from-string
-      "@@@@faosfasf adsfas \n asf @foo{asdf}\n@end verbatim\n"))
+  (test-equal
+   '("@@@@faosfasf adsfas " " NL\n" " asf @foo{asdf}" " NL\n")
+   (read-verbatim-body-from-string
+    "@@@@faosfasf adsfas \n asf @foo{asdf}\n@end verbatim\n"))
 
-    (assert-equal
-      (read-verbatim-body-from-string "@end verbatim \n@end verbatim\n")
-      '("@end verbatim " " NL\n"))))
+  (test-equal
+   '("@end verbatim " " NL\n")
+   (read-verbatim-body-from-string "@end verbatim \n@end verbatim\n")))
 
-(in-test-group read-arguments
-  (define (read-arguments-from-string str)
-    (read-arguments (open-string-input-port str) #\}))
+(define-test-case stexi-tests read-arguments ()
+  (let ()
+    (define (read-arguments-from-string str)
+      (read-arguments (open-string-input-port str) #\}))
 
-  (define (test str expected-res)
-    (assert-equal expected-res (read-arguments-from-string str)))
+    (define (test str expected-res)
+      (test-equal expected-res (read-arguments-from-string str)))
 
-  (define-each-test
     (test "}" '())
     (test "foo}" '("foo"))
     (test "foo,bar}" '("foo" "bar"))
     (test "    foo     ,    bar  }" '("foo" "bar"))
     (test " foo ,   , bar }" '("foo" #f "bar"))
     (test "foo,,bar}" '("foo" #f "bar"))
-    (assert-exception condition? (test "foo,,bar" 'foo))))
+    (test-exception condition? (test "foo,,bar" 'foo))))
 
-(in-test-group complete-start-command
-  (define (test command str)
-    (call-with-values
-        (lambda ()
-          (complete-start-command command (open-string-input-port str)))
-      list))
+(define-test-case stexi-tests complete-start-command ()
+  (let ()
+    (define (test command str)
+      (call-with-values
+          (lambda ()
+            (complete-start-command command (open-string-input-port str)))
+        list))
 
-  (define-each-test
-    (assert-equal
-     '(section () EOL-TEXT)
-     (test 'section "foo bar baz bonzerts"))
-    (assert-equal
-     '(deffnx ((category "Function") (name "foo") (arguments)) EOL-TEXT-ARGS)
-     (test 'deffnx "Function foo"))
-    (assert-exception condition? (test 'emph "no brace here"))
-    (assert-equal
-     '(emph () INLINE-TEXT)
-     (test 'emph "{foo bar baz bonzerts"))
-    (assert-equal
-     '(ref ((node "foo bar") (section "baz") (info-file "bonzerts"))
-           INLINE-ARGS)
-     (test 'ref "{ foo bar ,,  baz, bonzerts}"))
-    (assert-equal
-     '(node ((name "referenced node")) EOL-ARGS)
-     (test 'node " referenced node\n"))))
+    (test-equal
+        '(section () EOL-TEXT)
+      (test 'section "foo bar baz bonzerts"))
+    (test-equal
+        '(deffnx ((category "Function") (name "foo") (arguments)) EOL-TEXT-ARGS)
+      (test 'deffnx "Function foo"))
+    (test-exception condition? (test 'emph "no brace here"))
+    (test-equal
+        '(emph () INLINE-TEXT)
+      (test 'emph "{foo bar baz bonzerts"))
+    (test-equal
+        '(ref ((node "foo bar") (section "baz") (info-file "bonzerts"))
+              INLINE-ARGS)
+      (test 'ref "{ foo bar ,,  baz, bonzerts}"))
+    (test-equal
+        '(node ((name "referenced node")) EOL-ARGS)
+      (test 'node " referenced node\n"))))
 
 (define make-texinfo-token cons)
-(in-test-group read-char-data
+
+(define-test-case stexi-tests read-char-data ()
   (let* ((code (make-texinfo-token 'START 'code))
          (ref (make-texinfo-token 'EMPTY 'ref))
          (title (make-texinfo-token 'LINE 'title))
@@ -128,43 +131,42 @@
              (open-string-input-port str) expect-eof? preserve-ws? str-handler '()))
         (lambda (seed token)
           (let ((result (reverse seed)))
-            (assert-equal expected-data result)
-            (assert-equal expected-token token)))))
+            (test-equal expected-data result)
+            (test-equal expected-token token)))))
 
-    (define-each-test
-      ;; add some newline-related tests here (test "" #t #f '() (eof-object))
-      (test "foo bar baz" #t #f '("foo bar baz") (eof-object))
-      (assert-exception condition? (test "" #f #f '() (eof-object)))
-      (test "  " #t #f '("  ") (eof-object))
-      (test " @code{foo} " #f #f '(" ") code)
-      (test " @code" #f #f '(" ") code)
-      (test " {text here} asda" #f #f '(" ") (make-texinfo-token 'START '*braces*))
-      (test " blah blah} asda" #f #f '(" blah blah") (make-texinfo-token 'END #f)))))
+    ;; add some newline-related tests here (test "" #t #f '() (eof-object))
+    (test "foo bar baz" #t #f '("foo bar baz") (eof-object))
+    (test-exception condition? (test "" #f #f '() (eof-object)))
+    (test "  " #t #f '("  ") (eof-object))
+    (test " @code{foo} " #f #f '(" ") code)
+    (test " @code" #f #f '(" ") code)
+    (test " {text here} asda" #f #f '(" ") (make-texinfo-token 'START '*braces*))
+    (test " blah blah} asda" #f #f '(" blah blah") (make-texinfo-token 'END #f))))
 
-(in-test-group texinfo->stexinfo
-  (define (test str expected-res)
-    (assert-equal expected-res
-                  (texi->stexi (open-string-input-port str))))
-  (define (try-with-title title str)
-    (texi->stexi (open-string-input-port
-                  (string-append "foo bar baz\n@settitle " title "\n" str))))
-  (define (test-with-title title str expected-res)
-    (test (string-append "foo bar baz\n@settitle " title "\n" str)
-          expected-res))
-  (define (test-body str expected-res)
-    (assert-equal expected-res
-                  (cddr (try-with-title "zog" str))))
+(define-test-case stexi-tests texinfo->stexinfo ()
+  (let ()
+    (define (test str expected-res)
+      (test-equal expected-res
+        (texi->stexi (open-string-input-port str))))
+    (define (try-with-title title str)
+      (texi->stexi (open-string-input-port
+                    (string-append "foo bar baz\n@settitle " title "\n" str))))
+    (define (test-with-title title str expected-res)
+      (test (string-append "foo bar baz\n@settitle " title "\n" str)
+            expected-res))
+    (define (test-body str expected-res)
+      (test-equal expected-res
+        (cddr (try-with-title "zog" str))))
 
-  (define (list-intersperse src-l elem)
-    (if (null? src-l) src-l
-        (let loop ((l (cdr src-l)) (dest (cons (car src-l) '())))
-          (if (null? l) (reverse dest)
-              (loop (cdr l) (cons (car l) (cons elem dest)))))))
-  (define (join-lines . lines)
-    (apply string-append (list-intersperse lines "\n")))
+    (define (list-intersperse src-l elem)
+      (if (null? src-l) src-l
+          (let loop ((l (cdr src-l)) (dest (cons (car src-l) '())))
+            (if (null? l) (reverse dest)
+                (loop (cdr l) (cons (car l) (cons elem dest)))))))
+    (define (join-lines . lines)
+      (apply string-append (list-intersperse lines "\n")))
 
-  (define-each-test
-    (assert-exception condition? (test "@dots{}\n" '(texinfo (dots))))
+    (test-exception condition? (test "@dots{}\n" '(texinfo (dots))))
 
     (test "\\input texinfo\n@settitle my title\n@dots{}\n"
           '(texinfo (% (title "my title")) (para (dots))))
@@ -173,7 +175,7 @@
     (test-with-title "my title" "@dots{}"
                      '(texinfo (% (title "my title")) (para (dots))))
 
-    (assert-exception condition? (try-with-title "my title" "@dots{arg}"))
+    (test-exception condition? (try-with-title "my title" "@dots{arg}"))
 
     (test-body "@code{arg}"
                '((para (code "arg"))))
@@ -286,7 +288,7 @@
                             (item (para "one"))
                             (item (para "two"))
                             (item (para "three")))))
-    (assert-exception
+    (test-exception
      condition?
      (try-with-title "foo" (join-lines
                             "@enumerate string"
@@ -295,7 +297,7 @@
                             "@item three"
                             "@end enumerate"
                             )))
-    (assert-exception
+    (test-exception
      condition?
      (try-with-title "foo" (join-lines
                             "@itemize string"
@@ -372,8 +374,7 @@
                             (arguments "bar " (code "baz"))))
                  (para "text that should be in a para")))))
   
-
-(run-registered-tests)
+(run-test-suite stexi-tests)
 
 ;;; arch-tag: f98bded1-c044-457c-b358-7df6a397d648
 ;;; xml.texinfo.scm ends here
