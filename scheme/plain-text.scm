201924f3a1640a349f283bb718e3028c3fa3be05
Andreas Rottmann <rotty@debian.org>
2009-03-27 00:22:02 +0100

    Applied guile-lib 02515aec6e9 (licensing change to GPLv3+)
    Ignore-this: d54094607ccbc8eaffa57df3ade950d8
    
    darcs-hash:20090326232202-b4420-966384603e5b7f39252f9e1d47166b2c0794a26b.gz

--- scheme/plain-text.scm
+++ scheme/plain-text.scm
@@ -1,22 +1,18 @@
-;; guile-lib
-;; Copyright (C) 2003,2004 Andy Wingo <wingo at pobox dot com>
+;; (texinfo plain-text) -- rendering stexinfo as plain text
+;; Copyright (C) 2003,2004,2008  Andy Wingo <wingo at pobox dot com>
 
-;; This program is free software; you can redistribute it and/or    
-;; modify it under the terms of the GNU General Public License as   
-;; published by the Free Software Foundation; either version 2 of   
-;; the License, or (at your option) any later version.              
-;;                                                                  
-;; This program is distributed in the hope that it will be useful,  
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of   
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    
-;; GNU General Public License for more details.                     
-;;                                                                  
-;; You should have received a copy of the GNU General Public License
-;; along with this program; if not, contact:
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
 ;;
-;; Free Software Foundation           Voice:  +1-617-542-5942
-;; 59 Temple Place - Suite 330        Fax:    +1-617-542-2652
-;; Boston, MA  02111-1307,  USA       gnu@gnu.org
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
